38c230ca257cddc0a61031d7a8df6bc8aa87467c
백상수 <skygana@gmail.com>
2021-02-04 02:37:53 +0900

    메인페이지, 마이페이지 UI 구성

--- src/view/header.js
+++ src/view/header.js
@@ -1,38 +1,45 @@
-import React from 'react';
-import { 
-    BrowserRouter as Router, 
-    Route,
-    Link,
-} from 'react-router-dom';
-import GoogleButton from '../google-login/googleButton';
-import headerStyle from '../resources/style/css/header.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import GoogleButton from "../google-login/googleButton";
+import "../resources/style/css/header.css";
 
 const Header = () => {
-    return (
+  const [ auth, setAuth ] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="wrapper">
+        <h2>IIDX SKILL SIMULATOR</h2>
         <div>
-        <h1>IIDX SKILL SIMULATOR</h1>
-        <div><GoogleButton /></div>
-        <div>
-            <Route>
-                <div>
-                    <nav>
-                        <ul>
-                        <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/mypage">My page</Link>
-            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </Route>
+          <Route>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/home">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    {auth && <Link to="/mypage">My page</Link>}
+                  </li>
+                  <li>
+                    {auth && <Link to="/home">Upload</Link>}
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </Route>
         </div>
-        </div>
-    )
-}
+      </div>
+      <div className="user">
+        {/* <GoogleButton /> */}
+        {auth && <p>User님, 환영합니다.</p>}
+        <button onClick={() => { setAuth(!auth) }}> {auth ? "Logout" : "Login"} </button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
