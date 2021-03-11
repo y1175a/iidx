4b02d0f56696ca9b7afdd6c17e4e55aa8502936a
Sangsu <skygana@gmail.com>
2021-03-11 17:39:09 +0900

    commit before pull

--- src/sagas/authSaga.js
+++ src/sagas/authSaga.js
@@ -2,14 +2,16 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { authSlice } from '../reducers/auth';
 import * as authAPI from '../api/auth';
 
-// function* login(action) {
-//     try {
-//         const res = yield call(authAPI.login, action.payload);
-//         yield put(LOGIN_SUCCESS);
-//     } catch (err) {
+const { LOGIN } = authSlice.reducer;
 
-//     }
-// }
+function* login(action) {
+    try {
+        const res = yield call(authAPI.login());
+        yield put(authSlice);
+    } catch (err) {
+
+    }
+}
 
 
 
