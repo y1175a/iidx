24af318d15b8f1c2e76ad840746e04566943e6c9
Sangsu <skygana@gmail.com>
2021-03-20 07:39:19 +0900

    getUser 요청 생성

--- src/api/user.js
+++ src/api/user.js
@@ -2,3 +2,7 @@ import client from './client';
 
 export const getUsers = () => client.get(`/api/user`);
 
+export const getUser = ({ payload }) => {
+    const { uid } = payload;
+    return client.get(`/api/user/userid/${uid}`);
+};
