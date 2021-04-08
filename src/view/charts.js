9fea53e578bf0160e246360abe9148564a0e292b
Sangsu Baek <skygana@gmail.com>
2021-04-08 20:39:59 +0900

    페이지 이동기능 추가

--- src/view/charts.js
+++ src/view/charts.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { chartsActions } from '../reducers/charts';
+import queryString from 'query-string'; 
+import { Link } from "react-router-dom";
 
 const Charts = ({ match, location }) => {
     const { charts } = useSelector(({ charts }) => ({ charts }));
 
-    const [ page, setPage ] = useState(70);
+    const [ page, setPage ] = useState(1);
 
     const dispatch = useDispatch();
 
@@ -13,35 +15,54 @@ const Charts = ({ match, location }) => {
 
     useEffect(() => {
         dispatch(LOAD_CHARTS(page));
-        console.log(match, location)
     }, [page])
 
+    const onMoveToBackPage = () => {
+        setPage((page > 1) ? (page - 1) : page)
+    }
+
+    const onMoveToFrontPage = () => {
+        setPage((page < charts.last) ? (page + 1) : page)
+    }
+
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>Version</th>
-                    <th>Title</th>
-                    <th>Artist</th>
-                    <th>Difficulty</th>
-                    <th>Level</th>
-                    <th>Notes</th>
-                </tr>
-                {charts.list ? charts.list.map(chart => {
-                    return (
+        <>
+            {!charts.loading && <div>
+                <table>
+                    <thead>
                         <tr>
-                            <td className='attr1'>{chart.version}</td>
-                            <td className='attr2'>{chart.title}</td>
-                            <td className='attr3'>{chart.artist}</td>
-                            <td className='attr4'>{chart.diff}</td>
-                            <td className='attr5'>{chart.lv}</td>
-                            <td className='attr6'>{chart.notes}</td>
+                            <th>Version</th>
+                            <th>Title</th>
+                            <th>Artist</th>
+                            <th>Difficulty</th>
+                            <th>Level</th>
+                            <th>Notes</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                    {charts.list && charts.list.map((chart) => {
+                            return (<tr className="chart">
+                                <td className='attr1'>{chart.version}</td>
+                                <td className='attr2'>
+                                    <Link to={`/chart/${chart.id}`}>{chart.title}</Link>
+                                </td>
+                                <td className='attr3'>{chart.artist}</td>
+                                <td className='attr4'>{chart.diff}</td>
+                                <td className='attr5'>{chart.lv}</td>
+                                <td className='attr6'>{chart.notes}</td>
+                            </tr>)
+                        }
                     )
-                }) : <></>}
-            </table>
-            
-        </div>
+                    }
+                    </tbody>
+                </table>
+                <div>
+                    {page > 1 && <button onClick={onMoveToBackPage}>뒤로</button>}
+                    {page} / {charts.last}
+                    {page < charts.last && <button onClick={onMoveToFrontPage}>앞으로</button>}
+                </div>
+            </div>}
+        </>
     )
 }
 
