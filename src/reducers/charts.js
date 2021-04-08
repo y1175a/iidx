4dba941a4d83f3a9e062ce883dcfd50311c452e3
Sangsu Baek <skygana@gmail.com>
2021-04-08 20:49:07 +0900

    LOAD_CHARTS 액션 성공 시 수정

--- src/reducers/charts.js
+++ src/reducers/charts.js
@@ -4,12 +4,14 @@ const chartsSlice = createSlice({
   name: 'charts',
   initialState: {
     list: null,
+    last: 0,
     loading: false,
     error: null
   },
   reducers: {
     LOAD_CHARTS: {
       reducer: (state, action) => {
+        state.list = null;
         state.loading = true;
         state.error = null;
       },
@@ -19,7 +21,9 @@ const chartsSlice = createSlice({
     },
     LOAD_CHARTS_SUCCESS(state, { payload: { data } }) {
       state.loading = false;
-      state.list = data;
+      const { list, last } = data
+      state.list = list;
+      state.last = last;
     },
     LOAD_CHARTS_FAILURE(state, { payload }) {
       state.loading = false;
