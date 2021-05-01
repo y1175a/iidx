aea8b6a22a3892ca0117e02804ab99b2d5b81572
Sangsu Baek <skygana@gmail.com>
2021-05-01 21:57:10 +0900

    filter 추가

--- src/view/charts.js
+++ src/view/charts.js
@@ -1,8 +1,128 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { chartsActions } from '../reducers/charts';
-import queryString from 'query-string'; 
 import { Link } from "react-router-dom";
+
+const iidxVersionEnum = ['1st/substream', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+                         'RED', 'HAPPY SKY', 'DistorteD', 'GOLD', 'DJ TROOPERS', 'EMPRESS', 'SIRIUS',
+                         'Resort Anthem', 'Lincle', 'tricoro', 'SPADA', 'PENDUAL', 'copula', 'SINOBUZ',
+                         'CANNON BALLERS', 'Rootage', 'HEROIC VERSE', 'BISTROVER'];
+
+const iidxVersionContractionEnum = ['1st/sub', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+                         'RED', 'SKY', 'DD', 'GOLD', 'DJT', 'EMP', 'SIR',
+                         'RA', 'LC', 'tri', 'SPA', 'PEN', 'cop', 'SINO',
+                         'CAN', 'Root', 'HERO', 'BIS'];    
+                         
+const iidxDifficultyEnum = ['BEGINNER', 'NORMAL', 'HYPER', 'ANOTHER', 'LEGGENDARIA'];
+
+const iidxLevel = [...Array(12).keys()].map(value => value + 1);
+
+const Filter = () => {
+    const [ versionChecked, setVersionChecked ] = useState({});
+    const [ levelChecked, setLevelChecked ] = useState({});
+    const [ difficultyChecked, setDifficultyChecked ] = useState({});
+
+    useEffect(() => {
+        const versionCheckedStateInit = {};
+        const levelCheckedStateInit = {};
+        const difficultyCheckedStateInit = {};
+
+        iidxVersionContractionEnum.forEach(value => {
+            versionCheckedStateInit[`${value}`] = false;
+        });
+
+        iidxLevel.forEach(value => {
+            levelCheckedStateInit[`${value}`] = false;
+        });
+
+        iidxDifficultyEnum.forEach(value => {
+            difficultyCheckedStateInit[`${value}`] = false;
+        })
+
+        setVersionChecked(versionCheckedStateInit);
+        setLevelChecked(levelCheckedStateInit);
+        setDifficultyChecked(difficultyCheckedStateInit);
+    }, []);
+
+    useEffect(() => {
+        console.log(versionChecked);
+    }, [versionChecked]);
+
+    const onVersionCheckBoxHandler = (e) => {
+        setVersionChecked({...versionChecked, [e.target.value]: e.target.checked})
+    };
+
+    const onLevelCheckBoxHandler = (e) => {
+        setLevelChecked({...levelChecked, [e.target.value]: e.target.checked})
+    };
+
+    const onDifficultyCheckBoxHandler = (e) => {
+        setDifficultyChecked({...difficultyChecked, [e.target.value]: e.target.checked})
+    };
+
+    return (
+        <>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>시리즈별 구분</td>
+                        {iidxVersionContractionEnum.slice(0, 14).map(version => {
+                            return (
+                                <td>
+                                    <input type='checkbox'
+                                    name='version' 
+                                    value={version}
+                                    checked={versionChecked[version]} onClick={onVersionCheckBoxHandler} />
+                                    {versionChecked[version] ? <b>{version}</b> : version}
+                                </td>
+                            );
+                        })}
+                    </tr>
+                    <tr>
+                        <td></td>
+                        {iidxVersionContractionEnum.slice(14).map(version => 
+                                <td>
+                                    <input type='checkbox'
+                                    name='version' 
+                                    value={version}
+                                    checked={versionChecked[version]} onClick={onVersionCheckBoxHandler} />
+                                    {versionChecked[version] ? <b>{version}</b> : version}
+                                </td>
+                        )}
+                    </tr>
+                    <tr>
+                        <td>레벨별 구분</td>
+                        {iidxLevel.map(level => 
+                            <td>
+                                <input type='checkbox'
+                                    name='level' 
+                                    value={level}
+                                    checked={levelChecked[level]} onClick={onLevelCheckBoxHandler} />
+                                    {levelChecked[level] ? <b>{level}</b> : level}
+                            </td>
+                        )}
+                    </tr>
+                    <tr>
+                        <td>난이도별 구분</td>
+                        {iidxDifficultyEnum.map(diff => 
+                            <td>
+                                <input type='checkbox'
+                                    name='difficulty' 
+                                    value={diff}
+                                    checked={difficultyChecked[diff]} onClick={onDifficultyCheckBoxHandler} />
+                                    {difficultyChecked[diff] ? <b>{diff}</b> : diff}
+                            </td>
+                        )}
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+// const ChartList = () => {
+
+// }
 
 const Charts = ({ match, location }) => {
     const { charts } = useSelector(({ charts }) => ({ charts }));
@@ -27,6 +147,7 @@ const Charts = ({ match, location }) => {
 
     return (
         <>
+            <Filter />
             {!charts.loading && <div>
                 <table>
                     <thead>
