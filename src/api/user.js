ae8c8dd7c14bfe3edc794d22d90c7e45bd6a202f
Sangsu Baek <skygana@gmail.com>
2021-05-21 19:03:56 +0900

    Update user.js
    api 주소 변경

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,13 @@
 import client from './client';
 
-export const getUsers = () => client.get(`/api/user`);
+export const getUsers = () => client.get(`/api/users`);
 
 export const getUser = ({ payload }) => {
-    const { uid } = payload;
-    return client.get(`/api/user/userid/${uid}`);
+    const { id } = payload;
+    return client.get(`/api/user/${id}`);
+};
+
+export const updateUserInfo = ({ payload }) => {
+    const { id } = payload;
+    return client.patch(`/api/user/${id}`, payload);
 };
