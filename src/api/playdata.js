f3174ae09124b50df8bada26c83adf534c79af79
Sangsu Baek <skygana@gmail.com>
2021-08-07 20:42:30 +0900

    Update playdata.js

--- src/api/playdata.js
+++ src/api/playdata.js
@@ -2,5 +2,5 @@ import client from './client';
 
 export const getPlaydata = ({ payload }) => {
     const { chartId, userId } = payload;
-    return client.get(`/api/playdata/${chartId}/user/${userId}`);
+    return client.get(`/api/record/${chartId}/user/${userId}`);
 };
