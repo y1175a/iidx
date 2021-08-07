87c6d48e783e606a06d48456684deed1852a49ef
Sangsu Baek <skygana@gmail.com>
2021-08-07 20:42:26 +0900

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -4,10 +4,10 @@ export const getUsers = () => client.get(`/api/users`);
 
 export const getUser = ({ payload }) => {
     const { id } = payload;
-    return client.get(`/api/users/${id}`);
+    return client.get(`/api/user/${id}`);
 };
 
 export const updateUserInfo = ({ payload }) => {
     const { id } = payload;
-    return client.patch(`/api/users/${id}`, payload);
+    return client.patch(`/api/user/${id}`, payload);
 };
